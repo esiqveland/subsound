4616ecb013da43bf7d74772fb69eea898f6833f8
Eivind Siqveland <eivind@siqve.land>
2020-12-27 01:43:14 +0100

    album_page using CustomScrollView

--- lib/screens/login/album_page.dart
+++ lib/screens/login/album_page.dart
@@ -28,6 +28,8 @@ class WilderunAlbumScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MyScaffold(
+      appBar: null,
+      disableAppBar: true,
       body: Center(
         child: AlbumPage(
           ctx: client,
@@ -61,6 +63,8 @@ class AlbumScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MyScaffold(
+      appBar: null,
+      disableAppBar: true,
       body: Center(
         child: AlbumPage(
           ctx: client,
@@ -125,39 +129,51 @@ class AlbumView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: <Widget>[
-          Container(
-            child: CoverArtImage(
-              album.coverArtLink,
-              id: album.coverArtId,
-              height: 250.0,
-              width: null,
-            ),
-            margin: EdgeInsets.only(bottom: 10.0),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-            child: Text(
+    var expandedHeight = MediaQuery.of(context).size.height / 3;
+    return CustomScrollView(
+      physics: const BouncingScrollPhysics(),
+      slivers: [
+        SliverAppBar(
+          expandedHeight: expandedHeight,
+          stretch: true,
+          stretchTriggerOffset: 150,
+          centerTitle: false,
+          flexibleSpace: FlexibleSpaceBar(
+            titlePadding: EdgeInsets.only(top: 30.0, left: 5.0, bottom: 10.0),
+            centerTitle: false,
+            title: Text(
               album.name,
               style: TextStyle(
                 fontFamily: 'Roboto',
                 fontWeight: FontWeight.bold,
-                fontSize: 24.0,
+                fontSize: 16.0,
               ),
               overflow: TextOverflow.ellipsis,
+              maxLines: 1,
             ),
-            margin: EdgeInsets.only(bottom: 10.0),
-          ),
-          Expanded(
-            child: AlbumList(
-              album: album,
-              songs: album.songs,
+            background: CoverArtImage(
+              album.coverArtLink,
+              id: album.coverArtId,
+              height: expandedHeight,
+              width: null,
             ),
+            stretchModes: [
+              StretchMode.fadeTitle,
+              StretchMode.zoomBackground,
+            ],
           ),
-        ],
-      ),
+        ),
+        SliverList(
+          delegate: SliverChildListDelegate(
+              album.songs.map((s) => SongRow(song: s)).toList()),
+        ),
+        // Expanded(
+        //   child: AlbumList(
+        //     album: album,
+        //     songs: album.songs,
+        //   ),
+        // ),
+      ],
     );
   }
 }
@@ -189,20 +205,19 @@ class AlbumPageState extends State<AlbumPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: FutureBuilder<AlbumResult>(
-            future: load(this.albumId),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                if (snapshot.hasError) {
-                  return Text("${snapshot.error}");
-                } else {
-                  return AlbumView(album: snapshot.data);
-                }
-              } else {
-                return CircularProgressIndicator();
-              }
-            }));
+    return FutureBuilder<AlbumResult>(
+        future: load(this.albumId),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              return Center(child: Text("${snapshot.error}"));
+            } else {
+              return AlbumView(album: snapshot.data);
+            }
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        });
   }
 
   Future<AlbumResult> load(String albumId) {
