1c97634c39d8e0e2f5609c907b23e589a0bd51d0
Eivind Siqveland <eivind@siqve.land>
2021-01-16 17:24:55 +0100

    api: star/unstar

---
+++ lib/subsonic/requests/star.dart
@@ -0,0 +1,105 @@
+import 'dart:convert';
+
+import 'package:subsound/subsonic/base_request.dart';
+import 'package:subsound/subsonic/context.dart';
+import 'package:subsound/subsonic/response.dart';
+
+class StarResponse {}
+
+class SongId extends ItemId {
+  final String songId;
+  SongId({this.songId});
+
+  @override
+  String get getFieldName => "id";
+
+  @override
+  String get getId => this.songId;
+}
+
+class ArtistId extends ItemId {
+  final String artistId;
+  ArtistId({this.artistId});
+
+  @override
+  String get getFieldName => "artistId";
+
+  @override
+  String get getId => this.artistId;
+}
+
+class AlbumId extends ItemId {
+  final String albumId;
+  AlbumId({this.albumId});
+
+  @override
+  String get getFieldName => "albumId";
+
+  @override
+  String get getId => this.albumId;
+}
+
+abstract class ItemId {
+  String get getFieldName;
+  String get getId;
+}
+
+class StarItem extends BaseRequest<StarResponse> {
+  final ItemId id;
+  StarItem({this.id});
+
+  @override
+  Future<SubsonicResponse<StarResponse>> run(SubsonicContext ctx) async {
+    final uri = ctx.buildRequestUri("star", params: {
+      this.id.getFieldName: this.id.getId,
+    });
+
+    final response = await ctx.client.get(uri);
+    if (response.statusCode != 200) {
+      throw StateError("${response.statusCode}: " + response.body);
+    }
+
+    final data =
+        jsonDecode(utf8.decode(response.bodyBytes))['subsonic-response'];
+
+    if (data['status'] != 'ok') {
+      throw StateError("${response.statusCode}: " + response.body);
+    }
+
+    return SubsonicResponse(ResponseStatus.ok, ctx.version, StarResponse());
+  }
+
+  @override
+  String get sinceVersion => "1.8.0";
+}
+
+class UnstarResponse {}
+
+class UnstarItem extends BaseRequest<UnstarResponse> {
+  final ItemId id;
+  UnstarItem({this.id});
+
+  @override
+  Future<SubsonicResponse<UnstarResponse>> run(SubsonicContext ctx) async {
+    final uri = ctx.buildRequestUri("unstar", params: {
+      this.id.getFieldName: this.id.getId,
+    });
+
+    final response = await ctx.client.get(uri);
+    if (response.statusCode != 200) {
+      throw StateError("${response.statusCode}: " + response.body);
+    }
+
+    final data =
+        jsonDecode(utf8.decode(response.bodyBytes))['subsonic-response'];
+
+    if (data['status'] != 'ok') {
+      throw StateError("${response.statusCode}: " + response.body);
+    }
+
+    return SubsonicResponse(ResponseStatus.ok, ctx.version, UnstarResponse());
+  }
+
+  @override
+  String get sinceVersion => "1.8.0";
+}
