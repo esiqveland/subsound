43daca9749186fa19bfb2aae20e7b30d4b5cc122
Eivind Siqveland Larsen <eivind@siqve.land>
2021-01-17 12:18:45 +0100

    Create errors.dart

---
+++ lib/state/errors.dart
@@ -0,0 +1,11 @@
+import 'package:async_redux/async_redux.dart';
+import 'package:subsound/state/appstate.dart';
+
+class DisplayError extends ReduxAction<AppState> {
+  final String errorMessage;
+
+  DisplayError(this.errorMessage);
+
+  @override
+  Future<AppState> reduce() async {}
+}
