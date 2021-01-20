06750f252e99e8ca5efacee6a3a363bc8be67e15
Eivind Siqveland Larsen <eivind@siqve.land>
2021-01-20 17:42:14 +0100

    Create platformswitch.dart

---
+++ lib/utils/platformswitch.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class PlatformSwitch extends StatelessWidget {
+  final WidgetBuilder iosBuilder;
+  final WidgetBuilder androidBuilder;
+
+  const PlatformSwitch({
+    Key key,
+    this.iosBuilder,
+    this.androidBuilder,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    assert(theme.platform != null);
+    switch (theme.platform) {
+      case TargetPlatform.windows:
+      case TargetPlatform.linux:
+      case TargetPlatform.android:
+      case TargetPlatform.fuchsia:
+        return androidBuilder(context);
+      case TargetPlatform.macOS:
+      case TargetPlatform.iOS:
+        return iosBuilder(context);
+    }
+  }
+}
