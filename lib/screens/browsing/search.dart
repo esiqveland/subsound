9280443ef525f2c6194f21a0a7f54b97bb64a2d3
Eivind Siqveland <eivind@siqve.land>
2021-05-23 10:54:34 +0200

    Create search.dart

---
+++ lib/screens/browsing/search.dart
@@ -0,0 +1,136 @@
+import 'package:async_redux/async_redux.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:subsound/screens/browsing/home_page.dart';
+import 'package:subsound/screens/login/myscaffold.dart';
+import 'package:subsound/state/appcommands.dart';
+import 'package:subsound/state/appstate.dart';
+import 'package:subsound/state/playerstate.dart';
+import 'package:subsound/subsonic/requests/get_album.dart';
+import 'package:subsound/subsonic/requests/get_artist.dart';
+import 'package:subsound/subsonic/requests/search3.dart';
+
+class SearchScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MyScaffold(
+      appBar: AppBarSettings(disableAppBar: true),
+      body: (context) => Center(child: SearchField()),
+    );
+  }
+}
+
+class SearchField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StoreConnector<AppState, _SearchModel>(
+      vm: () => _SearchModelFactory(this),
+      builder: (context, model) {
+        return _SearchMusic(model: model);
+      },
+    );
+  }
+}
+
+class _SearchModelFactory extends VmFactory<AppState, SearchField> {
+  _SearchModelFactory(SearchField widget) : super(widget);
+
+  @override
+  _SearchModel fromStore() {
+    return _SearchModel(
+      currentPlayingId: state.playerState.currentSong?.id ?? '',
+      onPlaySong: (SongResult song, List<SongResult> playQueue) =>
+          dispatchFuture(PlayerCommandContextualPlay(
+        songId: song.id,
+        playQueue: playQueue,
+      )),
+      onPlayAlbum: (album) => dispatchFuture(PlayerCommandPlayAlbum(album)),
+      onSearch: (query) => dispatchFuture(SearchCommand(query))
+          .then((value) => currentState().dataState.searches.get(query))
+          .then((value) => value.data),
+    );
+  }
+}
+
+class _SearchModel extends Vm {
+  final Future<Search3Result> Function(String) onSearch;
+  final Future<void> Function(SongResult, List<SongResult>) onPlaySong;
+  final Future<void> Function(AlbumResultSimple) onPlayAlbum;
+  final String currentPlayingId;
+
+  _SearchModel({
+    required this.onSearch,
+    required this.onPlaySong,
+    required this.onPlayAlbum,
+    required this.currentPlayingId,
+  });
+}
+
+class _SearchMusic extends StatefulWidget {
+  final _SearchModel model;
+
+  const _SearchMusic({
+    Key? key,
+    required this.model,
+  }) : super(key: key);
+
+  @override
+  State<_SearchMusic> createState() {
+    return _SearchMusicState();
+  }
+}
+
+class _SearchMusicState extends State<_SearchMusic> {
+  Search3Result? result;
+  bool loading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    List<StarredItem> songResults =
+        result?.songs.map((e) => StarredItem(song: e)).toList() ?? [];
+
+    return Column(
+      children: [
+        CupertinoSearchTextField(
+          // onChanged: ,
+          onSubmitted: (val) {
+            this.setState(() {
+              result = null;
+              loading = true;
+            });
+            widget.model
+                .onSearch(val)
+                .then((response) => this.setState(() {
+                      result = response;
+                      response.songs.map((e) => StarredItem(song: e)).toList();
+                    }))
+                .whenComplete(() => this.setState(() {
+                      loading = false;
+                    }));
+          },
+        ),
+        SizedBox(height: 10),
+        Text("Artists"),
+        SizedBox(height: 10),
+        Text("Albums"),
+        SizedBox(height: 10),
+        Text("Songs"),
+        if (!loading)
+          StarredScrollView(
+            starred: songResults,
+            currentPlayingId: widget.model.currentPlayingId,
+            onPlay: (starredItem) {
+              if (starredItem.getSong() != null) {
+                var queue = songResults
+                    .where((element) => element.getSong() != null)
+                    .map((e) => e.getSong()!)
+                    .toList();
+                widget.model.onPlaySong(starredItem.getSong()!, queue);
+              } else if (starredItem.getAlbum() != null) {
+                widget.model.onPlayAlbum(starredItem.getAlbum()!);
+              } else {}
+            },
+          ),
+      ],
+    );
+  }
+}
