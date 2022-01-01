0de08184736e268ce0f9d4f7156722e5e8f67ced
Eivind Siqveland Larsen <eivind@siqve.land>
2022-01-01 12:30:49 +0100

    update slidable

--- lib/screens/login/album_page.dart
+++ lib/screens/login/album_page.dart
@@ -109,7 +109,6 @@ class SongRow extends StatelessWidget {
   final bool isPlaying;
   final Function(SongResult) onPlay;
   final Function(SongResult) onEnqueue;
-  final SlidableController slidableController;
 
   const SongRow({
     Key? key,
@@ -117,42 +116,48 @@ class SongRow extends StatelessWidget {
     required this.isPlaying,
     required this.onPlay,
     required this.onEnqueue,
-    required this.slidableController,
   }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
     return Slidable(
       key: Key(song.id),
-      actionPane: SlidableDrawerActionPane(),
-      direction: Axis.horizontal,
-      controller: slidableController,
-      dismissal: SlidableDismissal(
-        child: SlidableDrawerDismissal(),
-        onDismissed: (actionType) {},
-        onWillDismiss: (actionType) {
-          ScaffoldMessenger.of(context)
-              .showSnackBar(SnackBar(content: Text('Added to queue')));
-          onEnqueue(song);
-          return false;
-        },
-        closeOnCanceled: true,
-      ),
-      actions: [
-        IconSlideAction(
-          caption: 'Enqueue',
-          color: Colors.green,
-          icon: Icons.playlist_add,
-          onTap: () {
+      groupTag: '0',
+      startActionPane: ActionPane(
+        motion: const DrawerMotion(),
+        extentRatio: 1.0,
+        dismissible: DismissiblePane(
+          closeOnCancel: true,
+          onDismissed: () {
             ScaffoldMessenger.of(context)
-                .showSnackBar(SnackBar(content: Text("Added to queue")));
+                .showSnackBar(SnackBar(content: Text('Added to queue')));
             onEnqueue(song);
+            Slidable.of(context)?.close();
+          },
+          confirmDismiss: () async {
+            return true;
           },
         ),
-      ],
+        children: [
+          SlidableAction(
+            label: 'Enqueue',
+            backgroundColor: Colors.green,
+            icon: Icons.playlist_add,
+            onPressed: (context) {
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(SnackBar(content: Text("Added to queue")));
+              onEnqueue(song);
+              Slidable.of(context)?.close();
+            },
+          ),
+        ],
+      ),
+      direction: Axis.horizontal,
       child: ListTile(
         onTap: () {
           onPlay(song);
+          Slidable.of(context)?.close();
         },
         leading: Column(
           //mainAxisSize: MainAxisSize.min,
@@ -173,7 +178,9 @@ class SongRow extends StatelessWidget {
         title: Text(
           song.title,
           style: TextStyle(
-            color: isPlaying ? theme.colorScheme.secondary : theme.colorScheme.onPrimary,
+            color: isPlaying
+                ? theme.colorScheme.secondary
+                : theme.colorScheme.onPrimary,
             fontWeight: FontWeight.bold,
           ),
           textAlign: TextAlign.left,
@@ -191,7 +198,6 @@ class AlbumView extends StatelessWidget {
   final String? currentSongId;
   final Function(String songId, AlbumResult album) onPlay;
   final Function(SongResult song) onEnqueue;
-  final SlidableController _slidableController = SlidableController();
 
   AlbumView({
     Key? key,
@@ -205,143 +211,143 @@ class AlbumView extends StatelessWidget {
   Widget build(BuildContext context) {
     // var expandedHeight = MediaQuery.of(context).size.height / 3;
     var expandedHeight = 350.0;
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topRight,
-          end: Alignment.bottomCenter,
-          stops: [0.0, 0.8],
-          colors: [
-            Colors.blueGrey.withOpacity(0.7),
-            Colors.black.withOpacity(0.7),
-          ],
+
+    return SlidableAutoCloseBehavior(
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomCenter,
+            stops: [0.0, 0.8],
+            colors: [
+              Colors.blueGrey.withOpacity(0.7),
+              Colors.black.withOpacity(0.7),
+            ],
+          ),
         ),
-      ),
-      child: CustomScrollView(
-        primary: true,
-        physics: const BouncingScrollPhysics(
-          parent: AlwaysScrollableScrollPhysics(),
-        ),
-        slivers: <Widget>[
-          SliverAppBar(
-            backgroundColor: Colors.black54,
-            // foregroundColor: Colors.black54,
-            // shadowColor: Colors.black54,
-            expandedHeight: expandedHeight,
-            stretch: true,
-            centerTitle: false,
-            snap: false,
-            floating: false,
-            pinned: true,
-            primary: true,
-            flexibleSpace: FlexibleSpaceBar(
+        child: CustomScrollView(
+          primary: true,
+          physics: const BouncingScrollPhysics(
+            parent: AlwaysScrollableScrollPhysics(),
+          ),
+          slivers: <Widget>[
+            SliverAppBar(
+              backgroundColor: Colors.black54,
+              // foregroundColor: Colors.black54,
+              // shadowColor: Colors.black54,
+              expandedHeight: expandedHeight,
+              stretch: true,
               centerTitle: false,
-              //titlePadding: EdgeInsets.only(left: 5.0, bottom: 10.0),
-              title: Text(
-                album.name,
-                //textAlign: TextAlign.start,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 14.0,
-                ),
-                //overflow: TextOverflow.ellipsis,
-                //maxLines: 1,
-              ),
-              collapseMode: CollapseMode.parallax,
-              background: Stack(
-                fit: StackFit.expand,
-                children: [
-                  ConstrainedBox(
-                    constraints: BoxConstraints.tightFor(width: 450.0),
-                    child: CoverArtImage(
-                      album.coverArtLink,
-                      id: album.coverArtId,
-                      height: expandedHeight * 1.6,
-                      width: expandedHeight * 1.6,
-                      fit: BoxFit.cover,
-                    ),
+              snap: false,
+              floating: false,
+              pinned: true,
+              primary: true,
+              flexibleSpace: FlexibleSpaceBar(
+                centerTitle: false,
+                //titlePadding: EdgeInsets.only(left: 5.0, bottom: 10.0),
+                title: Text(
+                  album.name,
+                  //textAlign: TextAlign.start,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14.0,
                   ),
-                  const DecoratedBox(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        // end: Alignment(0.0, 0.0),
-                        begin: Alignment.bottomCenter,
-                        end: Alignment(0.0, 0.0),
-                        colors: <Color>[
-                          Color(0x60000000),
-                          Color(0x00000000),
-                        ],
+                  //overflow: TextOverflow.ellipsis,
+                  //maxLines: 1,
+                ),
+                collapseMode: CollapseMode.parallax,
+                background: Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    ConstrainedBox(
+                      constraints: BoxConstraints.tightFor(width: 450.0),
+                      child: CoverArtImage(
+                        album.coverArtLink,
+                        id: album.coverArtId,
+                        height: expandedHeight * 1.6,
+                        width: expandedHeight * 1.6,
+                        fit: BoxFit.cover,
                       ),
                     ),
-                  ),
-                ],
-              ),
-              stretchModes: [
-                StretchMode.fadeTitle,
-                StretchMode.zoomBackground,
-                //StretchMode.blurBackground,
-              ],
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Container(
-              height: 60,
-              padding: EdgeInsets.only(left: 20, top: 10),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              ArtistScreen(artistId: album.artistId),
+                    const DecoratedBox(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          // end: Alignment(0.0, 0.0),
+                          begin: Alignment.bottomCenter,
+                          end: Alignment(0.0, 0.0),
+                          colors: <Color>[
+                            Color(0x60000000),
+                            Color(0x00000000),
+                          ],
                         ),
-                      );
-                    },
-                    child: Text(
-                      album.artistName,
-                      style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
                     ),
-                  ),
-                  Text(
-                    "${album.year}",
-                    style: TextStyle(
-                        fontWeight: FontWeight.normal,
-                        color: Theme.of(context)
-                            .colorScheme
-                            .onPrimary
-                            .withOpacity(0.7)),
-                  ),
+                  ],
+                ),
+                stretchModes: [
+                  StretchMode.fadeTitle,
+                  StretchMode.zoomBackground,
+                  //StretchMode.blurBackground,
                 ],
               ),
             ),
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                final song = album.songs[index];
-                final isPlaying =
-                    currentSongId != null && currentSongId == song.id;
-                return SongRow(
-                  isPlaying: isPlaying,
-                  song: song,
-                  onEnqueue: (song) {
-                    _slidableController.activeState?.close();
-                    onEnqueue(song);
-                  },
-                  onPlay: (song) {
-                    _slidableController.activeState?.close();
-                    onPlay(song.id, album);
-                  },
-                  slidableController: _slidableController,
-                );
-              },
-              childCount: album.songs.length,
+            SliverToBoxAdapter(
+              child: Container(
+                height: 60,
+                padding: EdgeInsets.only(left: 20, top: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                ArtistScreen(artistId: album.artistId),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        album.artistName,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Text(
+                      "${album.year}",
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          color: Theme.of(context)
+                              .colorScheme
+                              .onPrimary
+                              .withOpacity(0.7)),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ),
-        ],
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  final song = album.songs[index];
+                  final isPlaying =
+                      currentSongId != null && currentSongId == song.id;
+                  return SongRow(
+                    isPlaying: isPlaying,
+                    song: song,
+                    onEnqueue: (song) {
+                      onEnqueue(song);
+                    },
+                    onPlay: (song) {
+                      onPlay(song.id, album);
+                    },
+                  );
+                },
+                childCount: album.songs.length,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
