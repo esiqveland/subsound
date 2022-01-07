02bc8efbf899653640c0e89106e816426242ee4b
Eivind Siqveland Larsen <eivind@siqve.land>
2022-01-07 19:47:20 +0100

    Create side_menu.dart

---
+++ lib/components/desktop/side_menu.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DesktopSideBar extends StatelessWidget {
+  const DesktopSideBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 280,
+      color: Colors.greenAccent,
+    );
+  }
+}
